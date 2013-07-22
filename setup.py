98df39b5f055e3b232e86c21d41ee849a4ad2101
Jakub Matys <jakub.matys@nodus.cz>
2013-07-22 08:33:18 +0200

    Used distutils instead of setuptools

--- setup.py
+++ setup.py
@@ -3,7 +3,24 @@
 #Copyright (c) 2012, Jakub Matys <matys.jakub@gmail.com>
 #All rights reserved.
 
-from setuptools import setup
+import sys
+from distutils.core import setup
+from distutils.command.install import install
+from distutils.errors import DistutilsPlatformError
+
+
+class _install(install):
+    def run(self):
+        try:
+            major = sys.version_info.major
+        except AttributeError:
+            major = 0
+
+        if major < 3:
+            raise DistutilsPlatformError('Use Python 3.0 or above')
+
+        super(_install, self).run()
+
 
 setup(name='GpuFanspeedController',
       version='0.2',
@@ -13,7 +30,6 @@ setup(name='GpuFanspeedController',
       author_email='matys.jakub@gmail.com',
       license='BSD',
       url='https://github.com/jakm/GpuFanspeedController',
-      install_requires=['python>=3.0'],
       packages=['gfcontroller',
                 'gfcontroller.backends',
                 'gfcontroller.backends.base',
@@ -24,5 +40,5 @@ setup(name='GpuFanspeedController',
       package_dir={'': 'src'},
       scripts=['src/GpuFanspeedController'],
       data_files=[('/etc', ['gfcontroller.cfg.sample']),
-                  ('/etc/systemd/system', ['gfcontroller.service'])]
-)
+                  ('/etc/systemd/system', ['gfcontroller.service'])],
+      cmdclass={'install': _install})
